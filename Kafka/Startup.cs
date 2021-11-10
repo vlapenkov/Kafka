3df0a296b11cbd32607733047e75890dcdabba1a
Vladimir Lapenkov <vlapenkov@ya.ru>
2021-11-10 13:44:56 +0300

    +swagger

--- Kafka/Startup.cs
+++ Kafka/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,11 @@ namespace Kafka
             services.AddHostedService<KafkaConsumerService>();
             services.AddSingleton<KafkaClientHandle>();
             services.AddSingleton<KafkaDependentProducer<Null, string>>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Kafka producer", Version = "v1" });
+            });
+
             // services.AddHostedService<TestHostedService>();
             // services.AddSingleton<IHostedService, KafkaConsumerHostedService>();
         }
@@ -27,6 +33,9 @@ namespace Kafka
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Kafka v1"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
